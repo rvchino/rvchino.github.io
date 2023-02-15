30daac7a04ae4387be51c79c72cfe3545157f9f3
rvchino <rvchino99@gmail.com>
2023-02-15 20:27:36 -0300

    correcciones a google

--- retoGoogle/app.js
+++ retoGoogle/app.js
@@ -2,23 +2,46 @@ let busqueda = document.getElementById('valor')
 let resultado = document.getElementById('resultados')
 let form = document.getElementById('form')
 let appearance = document.querySelector('.AAA')
+let algo = document.querySelectorAll('.resultados')
+let btn = document.querySelectorAll("btn")
+let botonera = document.getElementById('botonera')
+let grilla = document.querySelector('.grillaNav')
+let whole = document.querySelector('.wrapper')
 
 function logSubmit(event){
     event.preventDefault()
     let tipo = busqueda.value;
     let nodo = document.createTextNode(tipo)
     let nodito = document.createElement('li')
+    nodito.classList.add("resultados")
     nodito.appendChild(nodo)
     resultado.appendChild(nodito)
     appear()
     this.reset()
 }
 
+
+
 function appear(){
     appearance.style.display="block"
 }
 
+function disappear(){
+    appearance.style.display = "none"
+}
+
+document.addEventListener('click', function(event) {
+    // Elimina todos los elementos <li> dentro de la lista
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+        }
+    });
+
+
+window.addEventListener('click', disappear)
 
 form.addEventListener('submit', logSubmit, appear)
+
+
 
 
